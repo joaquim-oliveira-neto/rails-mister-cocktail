63762ac41551d05dcf72b3f8720a6c595c525e50
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-03 17:37:46 -0300

    O app funciona como manda a descrição do exercício. Até aqui o form para criar dose, está da cocktail page. Próximo passo é fazer ficar dentro

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: [:index, :show, :new, :create]  do
+    resources :doses, only: [:new, :create]
+  end
+  delete "doses/:id", to: "doses#destroy", as: :dose # or resources :doses, only: :destroy
+
 end
